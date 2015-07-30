b5dbd94baab3997704910ebbcd62fc501af8126f
David Ko <david.dk.ko@gmail.com>
2015-07-30 16:52:39 -0700

    Some changes...

--- tests/movewait.c.cpp
+++ tests/movewait.c.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "baromesh/linkbot.h"
 #include "myriolinkbot/myriolinkbot.h"
 
@@ -7,5 +8,11 @@ int main()
     linkbotMove((baromesh::Linkbot*)l, 0x07, 90, 90, 90);
     myRioLinkbotMoveWait(l, 0x03, 0.0);
     linkbotMove((baromesh::Linkbot*)l, 0x07, -90, -90, -90);
+    int moving;
+    while(1) {
+        myRioLinkbotIsMoving(l, 0x07, &moving);
+        std::cout << moving << std::endl;
+        if(!moving) break;
+    }
     myRioLinkbotMoveWait(l, 0x03, 0.0);
 }
